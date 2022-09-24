0fe787ead21f8c7e637edbdb8f673c852d023146
Sami Rherbi <sami0771@edu.zealand.dk>
2022-09-24 13:07:54 +0200

    Task 2

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class ClassRoom
+    {
+        public string ClassName { get; set; }
+        public List<Student> students { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+
+        }
+    }
+}
