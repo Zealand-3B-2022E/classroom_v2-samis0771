675b63373a51b9a9b988817b23a106f1f77ba6e0
Sami Rherbi <sami0771@edu.zealand.dk>
2022-09-24 12:55:37 +0200

    Task 1

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public int month { get; set; }
+        public int birthday { get; set; }
+
+        public Student(string Name, int Month, int Birthday)
+        {
+            this.name = Name;
+            this.month = Month;
+            this.birthday = Birthday;
+        }
+
+    }
+
+}
